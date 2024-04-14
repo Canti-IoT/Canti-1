a04e46115041f8407e4951396149d8877f015681
Catalin Plesu <catalinhimself@gmail.com>
2024-04-14 16:57:20 +0300

    feat: add debug flag and print functions

---
+++ include/debug.h
@@ -0,0 +1,12 @@
+#ifndef DEBUG_MACROS_H
+#define DEBUG_MACROS_H
+
+#if DEBUGENABLE == 1
+    #define DEBUGINIT(rate) USBSerial.begin(rate)
+#endif
+
+#if DEBUGENABLE == 1
+    #define DEBUG(...) USBSerial.print(__VA_ARGS__)
+#endif
+
+#endif /* DEBUG_MACROS_H */

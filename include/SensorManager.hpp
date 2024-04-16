6ac34bb17611da5fdef62b4c856b35b33b848251
Catalin Plesu <catalinhimself@gmail.com>
2024-04-16 18:59:53 +0300

    feat: create sensor manager

---
+++ include/SensorManager.hpp
@@ -0,0 +1,33 @@
+#ifndef SENSOR_MANAGER_H
+#define SENSOR_MANAGER_H
+
+#include "sensor/ParameterSensor.hpp"
+
+class SensorManager {
+public:
+    SensorManager(); // Constructor
+
+    // Function to add a sensor to the manager
+    void addSensor(ParameterType parameter, int recurrence, AbstractSensor& sensor);
+
+    // Function to get the sensor data for a given parameter
+    SensorData* getSensor(ParameterType parameter);
+
+    // Function to set recurrence for a sensor with a given index
+    void setRecurrenceWithIndex(ParameterType index, int recurrence);
+
+    // Function to begin reading for all sensors
+    void beginReadingAll();
+
+    // Function to get the parameter type by index
+    float getValue(int index);
+
+    bool validateIndex(int index);
+
+private:
+    static const int MAX_SENSORS = 8; // Maximum number of sensors
+    SensorData sensors[MAX_SENSORS]; // Array of SensorData
+    int currentSize; // Current size of the array
+};
+
+#endif
---
+++ src/SensorManager.cpp
@@ -0,0 +1,70 @@
+#include "SensorManager.hpp"
+#include <sensor/Bme680Adapter.hpp>
+
+SensorManager::SensorManager() {
+    currentSize = 0;
+
+    Bme680Adapter sensor();
+    // Initialize SensorData elements
+    for (int i = 0; i < MAX_SENSORS; ++i) {
+        sensors[i].parameter = ParameterType::NONE;
+        sensors[i].recurrence = 0;
+        sensors[i].sensor = &sensor; // Initialize sensor pointer to nullptr
+        sensors[i].value = 0.0f;
+    }
+}
+
+void SensorManager::addSensor(ParameterType parameter, int recurrence, AbstractSensor& sensor) {
+    if (currentSize < MAX_SENSORS) {
+        sensors[currentSize].parameter = parameter; 
+        sensors[currentSize].recurrence = recurrence; 
+        sensors[currentSize].sensor = sensor;
+        sensors[currentSize].value = 0.0f;
+        currentSize += 1;
+    } else {
+        // Handle error: array full
+    }
+}
+
+SensorData* SensorManager::getSensor(ParameterType parameter) {
+    for (int i = 0; i < currentSize; ++i) {
+        if (sensors[i].parameter == parameter) {
+            return &sensors[i];
+        }
+    }
+    return nullptr; // Return nullptr if sensor data not found
+}
+
+void SensorManager::setRecurrenceWithIndex(ParameterType index, int recurrence) {
+    for (int i = 0; i < currentSize; ++i) {
+        if (sensors[i].parameter == index) {
+            sensors[i].recurrence = recurrence;
+            break; // Exit loop once recurrence is set
+        }
+    }
+}
+
+void SensorManager::beginReadingAll() {
+    for (int i = 0; i < currentSize; ++i) {
+        sensors[i].sensor.beginReading(sensors[i].parameter);
+    }
+}
+
+float SensorManager::getValue(int index) {
+    for (int i =  0; i < currentSize; i++)
+    {
+        if (index == sensors[i].parameter){
+            return sensors[i].value;
+        }
+    }
+}
+
+bool SensorManager::validateIndex(int index){
+    for (int i =  0; i < currentSize; i++)
+    {
+        if (index == sensors[i].parameter){
+            return true;
+        }
+    }
+    return false;
+}

79358f79643413812a33de9507a6a77fa8068457
Catalin Plesu <catalinhimself@gmail.com>
2024-04-21 10:47:04 +0300

    feat: index service
    The bluetooth client can read sequentially supported parameters,
    and on writing in this characteristic the state will be reset.

--- src/ParameterIndexService.cpp
+++ src/ParameterIndexService.cpp
@@ -1,5 +1,9 @@
 #include "ParameterIndexService.hpp"
 #include <BLE2902.h>
+#include <SensorManager.hpp>
+#include <debug.h>
+
+SensorManager *sensorManager1 = nullptr;
 
 class IndexCharacteristicCallback : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic);
@@ -7,16 +11,29 @@ class IndexCharacteristicCallback : public BLECharacteristicCallbacks {
 };
 
 void IndexCharacteristicCallback::onWrite(BLECharacteristic *pCharacteristic) {
-    // Handle write event
+    // Get the value written to the characteristic
+    std::string value = pCharacteristic->getValue();
+    DEBUG("%s", value);
+    sensorManager1->getSupportedParameter(true); // Get the sensor value from SensorManager
+    // Handle the received value (parse, process, etc.)
+    // For example, if you expect a string:
+    // Serial.println("Received value: " + value);
 }
 
 void IndexCharacteristicCallback::onRead(BLECharacteristic *pCharacteristic) {
-    // Handle read event
+    // Read the value of the characteristic
+    // This depends on how you store the value you want to read
+    // For example, if you have a sensor value to send:
+    int sensorValue = sensorManager1->getSupportedParameter(); // Get the sensor value from SensorManager
+    pCharacteristic->setValue(sensorValue);
+    pCharacteristic->notify();
 }
 
 ParameterIndexService::ParameterIndexService(BLEServer *pServer) {
     // Initialize the service
     pService = pServer->createService(PARAMETER_INDEX_SERVICE_UUID);
+
+    sensorManager1 = &SensorManager::getInstance();
 
     // Create a characteristic
     pCharacteristic = pService->createCharacteristic(
@@ -26,13 +43,10 @@ ParameterIndexService::ParameterIndexService(BLEServer *pServer) {
         BLECharacteristic::PROPERTY_NOTIFY |
         BLECharacteristic::PROPERTY_INDICATE
     );
-
     // Set characteristic callbacks
     pCharacteristic->setCallbacks(new IndexCharacteristicCallback());
-
    // Create Client Characteristic Configuration Descriptor (CCC) for characteristic
     pCharacteristic->addDescriptor(new BLE2902());
-
     // Create Characteristic User Description Descriptor for characteristic
     BLEDescriptor pDescriptorUserDescription(BLEUUID((uint16_t)0x2901));
     pDescriptorUserDescription.setValue("index");

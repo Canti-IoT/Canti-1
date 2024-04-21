ef625f3081827979903a866c47db2cf6a598807f
Catalin Plesu <catalinhimself@gmail.com>
2024-04-21 11:57:54 +0300

    feat: add timestamp for debugging

--- include/debug.h
+++ include/debug.h
@@ -2,6 +2,7 @@
 #define DEBUG_MACROS_H
 
 #include <Arduino.h>
+#include <RTCSingleton.hpp>
 
 #if DEBUGENABLE == 1
     #define DEBUGINIT(rate) USBSerial.begin(rate)
@@ -10,5 +11,10 @@
 #if DEBUGENABLE == 1
     #define DEBUG(...) USBSerial.printf(__VA_ARGS__)
 #endif
+
+#if DEBUGENABLE == 1
+    #define TIMESTAMP() DEBUG("%d:%d:%d:%d - ", RTCSingleton::rtc.getHour(), RTCSingleton::rtc.getMinute(), RTCSingleton::rtc.getSecond(), RTCSingleton::rtc.getMillis())
+#endif
+  
 
 #endif /* DEBUG_MACROS_H */

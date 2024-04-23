9ebf82482f778ef11b855cca2947cef91547d32a
Catalin Plesu <catalinhimself@gmail.com>
2024-04-23 18:25:50 +0300

    feat: implement parameter-value service

--- src/bluetooth/ParameterValueService.cpp
+++ src/bluetooth/ParameterValueService.cpp
@@ -1,20 +1,40 @@
 #include "bluetooth/ParameterValueService.hpp"
 #include <BLE2902.h>
+#include <SensorManager.hpp>
+#include <debug.h>
 
-class ValueCharacteristicCallback : public BLECharacteristicCallbacks {
+SensorManager *sensorManager3 = nullptr;
+ParameterType p = ParameterType::NONE;
+
+class ValueCharacteristicCallback : public BLECharacteristicCallbacks
+{
     void onWrite(BLECharacteristic *pCharacteristic);
     void onRead(BLECharacteristic *pCharacteristic);
 };
 
-void ValueCharacteristicCallback::onWrite(BLECharacteristic *pCharacteristic) {
-    // Handle write event
+void ValueCharacteristicCallback::onWrite(BLECharacteristic *pCharacteristic)
+{
+    // Get the value written to the characteristic
+    std::string value = pCharacteristic->getValue();
+    p = static_cast<ParameterType>(value[0]);
+    float sensorValue = sensorManager3->getValue(p);
+    pCharacteristic->setValue(sensorValue);
+    pCharacteristic->notify();
 }
 
-void ValueCharacteristicCallback::onRead(BLECharacteristic *pCharacteristic) {
-    // Handle read event
+void ValueCharacteristicCallback::onRead(BLECharacteristic *pCharacteristic)
+{
+    // This depends on how you store the value you want to read
+    // For example, if you have a sensor value to send:
+    float sensorValue = sensorManager3->getValue(p);
+    pCharacteristic->setValue(sensorValue);
+    pCharacteristic->notify();
 }
 
-ParameterValueService::ParameterValueService(BLEServer *pServer) {
+ParameterValueService::ParameterValueService(BLEServer *pServer)
+{
+    sensorManager3 = &SensorManager::getInstance();
+
     // Initialize the service
     pService = pServer->createService(PARAMETER_VALUE_SERVICE_UUID);
 
@@ -22,15 +42,14 @@ ParameterValueService::ParameterValueService(BLEServer *pServer) {
     pCharacteristic = pService->createCharacteristic(
         VALUE_CHARACTERISTIC_UUID,
         BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_WRITE |
-        BLECharacteristic::PROPERTY_NOTIFY |
-        BLECharacteristic::PROPERTY_INDICATE
-    );
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_NOTIFY |
+            BLECharacteristic::PROPERTY_INDICATE);
 
     // Set characteristic callbacks
     pCharacteristic->setCallbacks(new ValueCharacteristicCallback());
 
-   // Create Client Characteristic Configuration Descriptor (CCC) for characteristic
+    // Create Client Characteristic Configuration Descriptor (CCC) for characteristic
     pCharacteristic->addDescriptor(new BLE2902());
 
     // Create Characteristic User Description Descriptor for characteristic
@@ -41,6 +60,7 @@ ParameterValueService::ParameterValueService(BLEServer *pServer) {
     pService->start();
 }
 
-void ParameterValueService::noitify(){
+void ParameterValueService::noitify()
+{
     pCharacteristic->notify();
 }

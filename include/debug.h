6d54e8ceac499149afe62ee4a6e8183cc2b25afe
Catalin Plesu <catalinhimself@gmail.com>
2024-05-07 12:21:39 +0300

    refactor: change timestamp format

--- include/debug.h
+++ include/debug.h
@@ -8,7 +8,7 @@
 
 #define DEBUGINIT(rate) USBSerial.begin(rate)
 #define DEBUG(...) USBSerial.printf(__VA_ARGS__)
-#define TIMESTAMP() DEBUG("%d:%d:%d:%d - ", RTCSingleton::rtc.getHour(), RTCSingleton::rtc.getMinute(), RTCSingleton::rtc.getSecond(), RTCSingleton::rtc.getMillis())
+#define TIMESTAMP() DEBUG("%2d/%2d/%4d%d:%d:%d:%d\n", RTCSingleton::rtc.getDay(), RTCSingleton::rtc.getMonth() + 1, RTCSingleton::rtc.getYear(), RTCSingleton::rtc.getHour(), RTCSingleton::rtc.getMinute(), RTCSingleton::rtc.getSecond(), RTCSingleton::rtc.getMillis())
 
 #else
 
